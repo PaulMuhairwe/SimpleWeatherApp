ec1eb13b4765b079a2b9cd3cf0f85a3f352f1d28
PaulMuhairwe <kpaul3642@gmail.com>
2024-03-29 10:18:52 +0300

    script

--- Weather/script.js
+++ Weather/script.js
@@ -5,6 +5,23 @@ function getWeather() {
     
 
     fetch (url)
-     .then
+     .then (response => response.json())
+     .then (data =>
+        {
+            const weatherInfo = document.getElementById("weatherInfo");
+            weatherInfo.innerHTML = `
+            <h1>${data.name}</h1>
+            <p>${data.weather[0].description}</p>
+            <p> Temeperature: ${data.main.temp} degrees celcious </p>
+            <p> Humidity: ${data.main.humidity} % </p>
+        `;
+            
+        }
+        )
+        .catch(error => {
+            console.error("Error fetching weather data:", error);
+            const weatherInfo = document.getElementById("weatherInfo");
+            weatherInfo.innerHTML="<p>Failed to fetch weather data. Please try again later.</p>";
+        });
     
 }
